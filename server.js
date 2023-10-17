5f378857ce20eaa6104f38179de20b71b50d8040
4clover88y <4clover88y@naver.com>
2023-10-17 23:32:45 +0900

    231017-1

--- server.js
+++ server.js
@@ -1,11 +1,77 @@
+const http = require('http');
 const express = require('express');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts')
+
 const app = express();
+const server = http.createServer(app);
+
+const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// app.use(express.static(__dirname + ''));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(expressLayouts);
+
+//express-ejs-layouts setting
+app.set('layout', 'layout');
+app.set("layout extractScripts", true);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-app.listen(port, () => {
-    console.log('서버가 실행됩니다. http://localhost:${port}');
-});
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
+app.get('/login', (req, res) => {
+  res.render('login');
+})
+
+
+// id
+app.get('/findId', (req, res) => {
+  res.render('findId');
+})
+
+app.get('/showId', (req, res) => {
+  res.render('showId');
+})
+
+
+// pw
+app.get('/findPw', (req, res) => {
+  res.render('findPw');
+})
+
+
+
+app.get('/board', (req, res) => {
+  res.render('board');
+})
+
+app.get('/board/Write', (req, res) => {
+  res.render('boardWrite');
+})
+
+app.get('/board/Post', (req, res) => {
+  res.render('boardPost');
+})
+
+
+app.get('/chat/list', (req, res) => {
+  res.render('chatList');
+})
